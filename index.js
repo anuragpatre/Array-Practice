5f3800f28288004dc11c5fd6f5600b8e0a69d034
anuragpatre <anuragpatre2005@gmail.com>
2024-08-13 00:09:29 +0530

    program to say  hello

---
+++ index.js
@@ -0,0 +1,9 @@
+
+//program to say hello to all elements
+
+const students = [ "Kohli" , "Rohit" , "Dhoni" , "Kapil" , "Rahul"]
+
+for(const stud of students )
+{
+       console.log(`Heyy hello ${stud}`)
+}
